03ed52692e4ff982186639bc67690927378edfc1
claud2005 <162583107+claud2005@users.noreply.github.com>
2025-06-13 15:27:09 +0100

    Atualização da app

---
+++ electron.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      contextIsolation: true,
+    },
+  });
+
+  // URL padrão do Expo Web
+  win.loadURL('https://gestor-cafe-v1.vercel.app');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
